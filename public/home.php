41ed127271d09e868b87a7b0fab502119ff791b3
danibert08 <berthida34@gmail.com>
2026-03-09 11:27:12 +0100

    update ruchaud presentaion

--- public/home.php
+++ public/home.php
@@ -31,8 +31,8 @@
             et optimisé pour le référencement google</p><br><br>
             <p>**************</p><br>
             <p><u>Tarif unique sans coûts cachés :</u></p><br><br>
-            <p>Une seule fois 79€ ttc pour la réalisation</p><br>
-            <p>12,99€/mois ttc pour l'hébergement, la maintenance, les petites modifications, les mises a jour</p><br>
+            <p>Une seule fois 69€ pour la réalisation</p><br>
+            <p>12,99€/mois pour l'hébergement, la maintenance, les petites modifications, les mises a jour</p><br>
             <p>**************</p>
         </div>
 
